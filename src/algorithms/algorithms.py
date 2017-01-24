3d191e1c9b7a06ef38a60e5f98fd853236259cff
Patryk Łucka <patryk.lucka@gmail.com>
2017-01-24 19:21:43 +0100

    few new statistics

--- src/algorithms/algorithms.py
+++ src/algorithms/algorithms.py
@@ -49,13 +49,39 @@ class AlgorithmsComparer(object):
 
         #Counting
         k_means_clusters_size = [0]*n_clusters
+        k_means_min = len(data)
+        k_means_max = 0
+        k_means_sum = 0
         for element in k_means_clusters:
             k_means_clusters_size[element] = k_means_clusters_size[element] + 1
 
+        for size in k_means_clusters_size:
+            if size < k_means_min:
+                k_means_min = size
+            if size > k_means_max:
+                k_means_max = size
+            k_means_sum = k_means_sum + size
+
+        k_means_mean = k_means_sum/n_clusters
+        k_means_std = np.std(np.array(k_means_clusters_size))
+
         som_clusters_size = [0]*n_clusters
+        som_min = len(data)
+        som_max = 0
+        som_sum = 0
         for element in som_clusters:
             transposed_cluster = som.winner(k_means_cluster_centers[element])[1]
             som_clusters_size[transposed_cluster] = som_clusters_size[transposed_cluster] + 1
+
+        for size in som_clusters_size:
+            if size < som_min:
+                som_min = size
+            if size > som_max:
+                som_max = size
+            som_sum = som_sum + size
+
+        som_mean = som_sum / n_clusters
+        som_std = np.std(np.array(som_clusters_size))
 
         different = (som_clusters == -1)
         differentCount = 0
@@ -66,5 +92,17 @@ class AlgorithmsComparer(object):
                 if value:
                     differentCount = differentCount + 1
 
-        result = ResultStatistics(k_means_clusters_size, som_clusters_size, t_batch, t_batch2, differentCount)
+        result = ResultStatistics(k_means_clusters_size, som_clusters_size, t_batch, t_batch2, differentCount, k_means_min, som_min, k_means_max, som_max, k_means_mean, som_mean, k_means_std, som_std)
+        print('k-means')
+        print(k_means_clusters_size)
+        print('min - ' + str(k_means_min))
+        print('max - ' + str(k_means_max))
+        print('mean - ' + str(k_means_mean))
+        print('std - ' + str(k_means_std))
+        print('som')
+        print(som_clusters_size)
+        print('min - ' + str(som_min))
+        print('max - ' + str(som_max))
+        print('mean - ' + str(som_mean))
+        print('std - ' + str(som_std))
         return result
